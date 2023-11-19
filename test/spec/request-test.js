4f748c33e42e0ec1c9f79f1f7af35d507e1b358a
John Gozde <john@gozde.ca>
2023-11-18 20:19:24 -0700

    Add tests, rest of public API

--- test/spec/request-test.js
+++ test/spec/request-test.js
@@ -146,3 +146,377 @@ describe("Request-Promise-Native", function () {
         }).to.not.throw();
     });
 });
+
+describe("ported from request-promise-core", () => {
+    var stopServer = null;
+
+    before(function (done) {
+        startServer(4000, function (stop) {
+            stopServer = stop;
+            done();
+        });
+    });
+
+    after(function (done) {
+        stopServer(done);
+    });
+
+    describe("plumbing.init", () => {
+        it("that sets up the default options", function () {
+            const req = rp("http://localhost:4000/200");
+
+            delete req._rp_options.callback;
+            expect(req._rp_options).to.eql({
+                simple: true,
+                resolveWithFullResponse: false,
+                transform: undefined,
+                transform2xxOnly: false,
+                uri: "http://localhost:4000/200",
+            });
+        });
+
+        it("that forwards any custom options", function () {
+            const req = rp("http://localhost:4000/200", {
+                custom: "test",
+            });
+
+            delete req._rp_options.callback;
+
+            expect(req._rp_options).to.eql({
+                custom: "test",
+                simple: true,
+                resolveWithFullResponse: false,
+                transform: undefined,
+                transform2xxOnly: false,
+                uri: "http://localhost:4000/200",
+            });
+        });
+
+        it("that allows custom values for the Request-Promise options", function () {
+            var customTransform = function () {};
+
+            const req = rp("http://localhost:4000/200", {
+                simple: false,
+                resolveWithFullResponse: true,
+                transform: customTransform,
+                transform2xxOnly: true,
+            });
+
+            delete req._rp_options.callback;
+
+            expect(req._rp_options).to.eql({
+                simple: false,
+                resolveWithFullResponse: true,
+                transform: customTransform,
+                transform2xxOnly: true,
+                uri: "http://localhost:4000/200",
+            });
+        });
+
+        it("that converts the method to upper case", function () {
+            const req = rp("http://localhost:4000/200", {
+                method: "get",
+            });
+
+            expect(req._rp_options.method).to.eql("GET");
+        });
+
+        it("that applies a default transform for HEAD requests", function () {
+            const req = rp("http://localhost:4000/200", {
+                method: "head",
+            });
+
+            expect(req._rp_options.transform).to.eql(
+                rp.Request.defaultTransformations.HEAD
+            );
+        });
+
+        it("that keeps the already existing callback", function () {
+            var alreadyExistingCallback = function () {};
+
+            const req = rp("http://localhost:4000/200", {
+                callback: alreadyExistingCallback,
+            });
+
+            expect(req._rp_callbackOrig).to.eql(alreadyExistingCallback);
+        });
+    });
+
+    describe("doing requests", function () {
+        it("that is successful", function (done) {
+            rp("http://localhost:4000/200")
+                .then(function (body) {
+                    expect(body).to.eql("GET /200");
+                    done();
+                })
+                .catch(function (err) {
+                    done(err);
+                });
+        });
+
+        it("that is successful with non-default options", function (done) {
+            rp({
+                uri: "http://localhost:4000/404",
+                simple: false,
+                resolveWithFullResponse: true,
+                transform: function () {
+                    return "must not be called";
+                },
+                transform2xxOnly: true,
+            })
+                .then(function (response) {
+                    expect(response.body).to.eql("GET /404");
+                    done();
+                })
+                .catch(function (err) {
+                    done(err);
+                });
+        });
+
+        it('with method "post" in lower case', function (done) {
+            rp({
+                method: "post",
+                uri: "http://localhost:4000/200",
+                body: {
+                    a: "b",
+                },
+                json: true,
+            })
+                .then(function (body) {
+                    expect(body).to.eql('POST /200 - {"a":"b"}');
+                    done();
+                })
+                .catch(function (err) {
+                    done(err);
+                });
+        });
+
+        it("with a transform function", function (done) {
+            rp({
+                method: "post",
+                uri: "http://localhost:4000/200",
+                body: {
+                    a: "b",
+                },
+                json: true,
+                transform: function (body, response, resolveWithFullResponse) {
+                    return body.split("").reverse().join("");
+                },
+            })
+                .then(function (body) {
+                    expect(body).to.eql(
+                        'POST /200 - {"a":"b"}'.split("").reverse().join("")
+                    );
+                    done();
+                })
+                .catch(function (err) {
+                    done(err);
+                });
+        });
+
+        it("that is successfully redirected", function (done) {
+            rp("http://localhost:4000/301")
+                .then(function (body) {
+                    expect(body).to.eql("GET /200");
+                    done();
+                })
+                .catch(function (err) {
+                    done(err);
+                });
+        });
+
+        it("that fails", function (done) {
+            rp("http://localhost:1/200")
+                .then(function () {
+                    done(new Error("Expected promise to be rejected."));
+                })
+                .catch(function (err) {
+                    expect(err instanceof errors.RequestError).to.eql(true);
+                    done();
+                });
+        });
+
+        it("that gets a 500 response", function (done) {
+            rp("http://localhost:4000/500")
+                .then(function () {
+                    done(new Error("Expected promise to be rejected."));
+                })
+                .catch(function (err) {
+                    expect(err instanceof errors.StatusCodeError).to.eql(true);
+                    done();
+                });
+        });
+
+        describe("callbacks", () => {
+            it("200", function (done) {
+                var callbackWasCalled = false;
+
+                rp("http://localhost:4000/200", function () {
+                    callbackWasCalled = true;
+                })
+                    .then(function (body) {
+                        expect(body).to.eql("GET /200");
+                        expect(callbackWasCalled).to.eql(true);
+                        done();
+                    })
+                    .catch(function (err) {
+                        done(err);
+                    });
+            });
+
+            it("201", function (done) {
+                var callbackWasCalled = false;
+
+                rp("http://localhost:4000/201", function () {
+                    callbackWasCalled = true;
+                })
+                    .then(function (body) {
+                        expect(body).to.eql("GET /201");
+                        expect(callbackWasCalled).to.eql(true);
+                        done();
+                    })
+                    .catch(function (err) {
+                        done(err);
+                    });
+            });
+
+            it("2xx with full response", function (done) {
+                var callbackWasCalled = false;
+
+                rp(
+                    "http://localhost:4000/201",
+                    { resolveWithFullResponse: true },
+                    function () {
+                        callbackWasCalled = true;
+                    }
+                )
+                    .then(function (response) {
+                        expect(response.body).to.eql("GET /201");
+                        expect(callbackWasCalled).to.eql(true);
+                        done();
+                    })
+                    .catch(function (err) {
+                        done(err);
+                    });
+            });
+
+            it("rejects non-2xx in simple mode", function (done) {
+                var callbackWasCalled = false;
+
+                rp("http://localhost:4000/404", function () {
+                    callbackWasCalled = true;
+                })
+                    .then(function (response) {
+                        done(new Error("Expected promise to be rejected."));
+                    })
+                    .catch(function (err) {
+                        expect(err instanceof errors.StatusCodeError).to.eql(
+                            true
+                        );
+                        expect(err.name).to.eql("StatusCodeError");
+                        expect(err.statusCode).to.eql(404);
+                        expect(err.message).to.eql('404 - "GET /404"');
+                        expect(err.error).to.eql("GET /404");
+                        expect(callbackWasCalled).to.eql(true);
+                        done();
+                    });
+            });
+
+            it("resolves non-2xx in non-simple mode", function (done) {
+                var callbackWasCalled = false;
+
+                rp("http://localhost:4000/404", {
+                    simple: false,
+                    callback: () => {
+                        callbackWasCalled = true;
+                    },
+                })
+                    .then(function (body) {
+                        expect(body).to.eql("GET /404");
+                        expect(callbackWasCalled).to.eql(true);
+                        done();
+                    })
+                    .catch(function (err) {
+                        done(err);
+                    });
+            });
+        });
+    });
+
+    describe("should support Request's", function () {
+        it("method shortcuts", function (done) {
+            rp.post({
+                uri: "http://localhost:4000/404",
+                body: {
+                    a: "b",
+                },
+                json: true,
+                simple: false, // <-- ensures that parameter is forwarded
+            })
+                .then(function (body) {
+                    expect(body).to.eql('POST /404 - {"a":"b"}');
+                    done();
+                })
+                .catch(function (err) {
+                    done(err);
+                });
+        });
+
+        it(".defaults(...) feature", function (done) {
+            var rpSimpleOff = rp.defaults({ simple: false });
+
+            rpSimpleOff({
+                uri: "http://localhost:4000/404",
+                resolveWithFullResponse: true,
+            })
+                .then(function (response) {
+                    expect(response.body).to.eql("GET /404");
+                    done();
+                })
+                .catch(function (err) {
+                    done(err);
+                });
+        });
+
+        it(".defaults(...) feature using it multiple times", function (done) {
+            var rpSimpleOff = rp.defaults({ simple: false });
+            var rpSimpleOffWithFullResp = rpSimpleOff.defaults({
+                resolveWithFullResponse: true,
+            });
+
+            rpSimpleOffWithFullResp("http://localhost:4000/404")
+                .then(function (response) {
+                    expect(response.body).to.eql("GET /404");
+                    done();
+                })
+                .catch(function (err) {
+                    done(err);
+                });
+        });
+
+        it("event emitter", function (done) {
+            rp("http://localhost:4000/200").on(
+                "complete",
+                function (httpResponse, body) {
+                    expect(httpResponse.statusCode).to.eql(200);
+                    expect(body).to.eql("GET /200");
+                    done();
+                }
+            );
+        });
+
+        it("main function to take extra options as the second parameter", function (done) {
+            rp("http://localhost:4000/200", {
+                method: "POST",
+                json: { foo: "bar" },
+            })
+                .then(function (body) {
+                    expect(body).to.eql('POST /200 - {"foo":"bar"}');
+                    done();
+                })
+                .catch(function (err) {
+                    done(err);
+                });
+        });
+    });
+});
